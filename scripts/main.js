2fd911d6740cb9dd8609eb4da9ee0fe7f81aa709
Peter <peter.bedford2@gmail.com>
2022-03-13 11:50:02 +0000

    A little tidying up

--- scripts/main.js
+++ scripts/main.js
@@ -1,79 +1,88 @@
-debugMode = true;
-StdLength = 500;
-elements =  document.getElementsByClassName("StaticOuterBox");//['.OuterBoxOne', '.OuterBoxTwo', '.OuterBoxThree', '.OuterBoxFour'];
- test = jQuery.makeArray( elements );
- screens = ['.OuterBoxOne', '.OuterBoxTwo', '.OuterBoxThree', '.OuterBoxFour'];
+const debugMode = true;
+const StdLength = 500; //Pretty sure if you change this the website will go on fire.
+const screenNames = ['.OuterBoxOne', '.OuterBoxTwo', '.OuterBoxThree', '.OuterBoxFour'];
 
 $(function () {
 
+  //stores direct refrences to all screens in the docment in an array
+  storedScreens = StoreScreens(screenNames);
 
   //sets the document lendth relative to how many screens are used.
-  docLen = ( screens.length * 1000)
+  docLen = ( screenNames.length * 1000)
   $(".mainContainer").css("height", docLen+"px");
-
 
   //functions called when scrolling happens
   $(window).scroll(function() {
-    crossFade(screens, StdLength)
+    crossFade(screenNames, StdLength)
     Fade('.murloc', 1200)
-    screens.forEach(ShowHide);
+    storedScreens.forEach(ShowHide); //-- remove the need for this asap.
   });
 
   $(".murloc").click(function(){
-    console.log("asdasd");
     $(".murlocHead").animate({top: '11%'}).animate({left: '60%'}, "slow").animate({top: '20%'}, function () { $(this).removeAttr('style'); });
   });
 
+  //Looks up and stores direct refrences to screens. Doing this just once to try improve performance.
+  function StoreScreens(classArray){
 
-  //Don't ask - https://i.imgur.com/Lda77tU.jpg
+    elementArray = new Array();
+
+    classArray.forEach(function(item){
+      elementArray.push($(item));
+    })
+
+    return elementArray;
+  }
+
+  //https://i.imgur.com/Lda77tU.jpg
   function ShowHide(item){
-      if($(item).css("opacity") == 0){
-        $(item).hide();
+      if(item.css("opacity") == 0){
+        item.hide();
       }else{
-        $(item).show();
+        item.show();
       }
   }
 
   function crossFade(elements, len) {
 
     //stores the current page position
-    PagePosition = document.documentElement.scrollTop;
+    pagePosition = document.documentElement.scrollTop;
 
-    //sets which sceens are active based on page position
-    if(PagePosition < 500){
+    //calculates the index of the currently active pages
+    if(pagePosition < StdLength){
       indexPos = 0;
     }else{
-      indexPos = Math.floor(PagePosition / 1000)
+      indexPos = Math.floor(pagePosition / 1000)
     }
-    element1 = elements[indexPos];
-    element2 = elements[indexPos+1];
-    
+
+    //store references to the active pages
+    element1 = storedScreens[indexPos];
+    element2 = storedScreens[indexPos+1];
+
     //sets the fade in and out points
-    fadeBase = (indexPos * StdLength * 2) + 500;
+    fadeBase = (indexPos * StdLength * 2) + StdLength;
     fadeStart = fadeBase;
-    fadeEnd = fadeBase + 500;
+    fadeEnd = fadeBase + StdLength;
 
 
-
-    //an attempt to make sure pages are fully visable or fully hidden
-    if (PagePosition < fadeStart) {
-      position = 0;
-    }else if(PagePosition > fadeEnd){
-      position = 1;
+    //an attempt to make sure pages are fully visable or fully hidden --- REWRITE THIS!!!!!
+    if (pagePosition < fadeStart) {
+      opacity = 0;
+    }else if(pagePosition > fadeEnd){
+      opacity = 1;
     }else{ 
-        position = (PagePosition - fadeStart) / (fadeEnd - fadeStart)
+      opacity = (pagePosition - fadeStart) / (fadeEnd - fadeStart)
     }
-    if(indexPos => 1){
-      $(elements[indexPos-1]).css({ opacity: 0 });
+    if(indexPos > 1){
+      storedScreens[indexPos-1].css({ opacity: 0 });
     }
-
   
-      
-    if(debugMode){console.log("Element1: " + element1 + " Element2: " + element2 + " Absolute position: " + PagePosition + " Opacity: " + position +" FadeBase: " + fadeBase + "Fade Start: " + fadeStart + "Fade End: " + fadeEnd)}
+    if(debugMode){console.log("Element1: " + element1 + " Element2: " + element2 + " Absolute position: " + pagePosition + " Opacity: " + opacity +" FadeBase: " + fadeBase + "Fade Start: " + fadeStart + "Fade End: " + fadeEnd)}
     
-    //update opacity
-    $(element1).css({ opacity: 1 - position });
-    $(element2).css({ opacity: position });
+    //update opacity while checking if screens are out of bounds (before first screen, after end screen).
+    if(indexPos > 1){element1.css({ opacity: 1 - opacity });}
+    if(indexPos + 1 < storedScreens.length){element2.css({ opacity: opacity });}
+    
 
   }
 
